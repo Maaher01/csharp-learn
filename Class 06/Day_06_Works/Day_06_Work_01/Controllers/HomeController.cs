2e6fbe96a33ebe785ff652f600eb70243504c228
unknown <maaher1145@gmail.com>
2024-01-26 14:15:13 +0600

    Day 5 and 6

---
+++ Class 06/Day_06_Works/Day_06_Work_01/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Day_06_Work_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Author = "Maaher Hossain";
+            ViewData["City"] = "Dhaka";
+
+            return View( new List<Student> 
+            { 
+                new Student { Id = 1, Name = "John" },
+                new Student { Id = 2, Name = "Jack" }
+            });
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
