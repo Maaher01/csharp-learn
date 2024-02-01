e06a9734d0578bc06d1d1983cab253ff4a1ea396
unknown <maaher1145@gmail.com>
2024-02-02 01:45:36 +0600

    CRUD operation started

---
+++ Class 07/Day_07_Works/Day_07_Work_01/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Day_07_Work_01
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Class 07/Day_07_Works/Day_07_Work_01/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Day_07_Work_01.Models;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Day_07_Work_01.Controllers
+{
+    public class ProductsController : Controller
+    {
+        readonly StoreDbContext db = new StoreDbContext();
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Products.ToListAsync());
+        }
+    }
+}
